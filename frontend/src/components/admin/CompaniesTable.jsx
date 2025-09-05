546ad54ade55bb16097d33067de2fa95c067cd69
vedant_dagade <vedantdagade21@gmail.com>
2025-09-05 17:14:32 +0530

    issue fixes basics

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -26,29 +26,31 @@ const CompaniesTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableCell>
-            <Avatar className="w-11 h-11 rounded-full">
-              <AvatarImage
-                src="https://imgs.search.brave.com/Y_hU5wT4IYRa1yUzMC0a8yvx4jxxLEb8rXWL_DYiqZ8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzEyLzg4LzkxLzkx/LzM2MF9GXzEyODg5/MTkxMDZfS3hMRG1U/ZmU3ZkZLRVBKMVQ3/WDFjRnVzRngzUUJF/ZTIuanBn"
-                alt="Company Logo"
-              />
-            </Avatar>
-          </TableCell>
-          <TableCell>Company Name</TableCell>
-          <TableCell>5/08/2025</TableCell>
-          <TableCell className="text-right cursor-pointer">
-            <Popover>
-              <PopoverTrigger>
-                <MoreHorizontal />
-              </PopoverTrigger>
-              <PopoverContent className="w-32">
-                <div className="flex items-center gap-2 w-fit cursor-pointer">
-                  <Edit2 className="w-4"/>
-                  <span>Edit To</span>
-                </div>
-              </PopoverContent>
-            </Popover>
-          </TableCell>
+          <TableRow>
+            <TableCell>
+              <Avatar className="w-11 h-11 rounded-full">
+                <AvatarImage
+                  src="https://imgs.search.brave.com/Y_hU5wT4IYRa1yUzMC0a8yvx4jxxLEb8rXWL_DYiqZ8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzEyLzg4LzkxLzkx/LzM2MF9GXzEyODg5/MTkxMDZfS3hMRG1U/ZmU3ZkZLRVBKMVQ3/WDFjRnVzRngzUUJF/ZTIuanBn"
+                  alt="Company Logo"
+                />
+              </Avatar>
+            </TableCell>
+            <TableCell>Company Name</TableCell>
+            <TableCell>5/08/2025</TableCell>
+            <TableCell className="text-right cursor-pointer">
+              <Popover>
+                <PopoverTrigger>
+                  <MoreHorizontal />
+                </PopoverTrigger>
+                <PopoverContent className="w-32">
+                  <div className="flex items-center gap-2 w-fit cursor-pointer">
+                    <Edit2 className="w-4" />
+                    <span>Edit To</span>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </TableCell>
+          </TableRow>
         </TableBody>
       </Table>
     </div>

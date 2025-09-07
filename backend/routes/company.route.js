91befe319a9300b07b894454f157a198a7807a4e
vedant_dagade <vedantdagade21@gmail.com>
2025-09-07 20:58:53 +0530

    Company Setup completed

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,14 +1,21 @@
 import express from "express";
 
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import {
+  getCompany,
+  getCompanyById,
+  registerCompany,
+  updateCompany,
+} from "../controllers/company.controller.js";
+
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 // ✅ Correct usage
-router.post("/register", isAuthenticated, registerCompany);
+router.post("/register", isAuthenticated, singleUpload, registerCompany);
 router.get("/get", isAuthenticated, getCompany);
 router.get("/get/:id", isAuthenticated, getCompanyById); // logout usually a GET route
-router.put("/update/:id", isAuthenticated, updateCompany);
+router.put("/update/:id", isAuthenticated, singleUpload, updateCompany);
 
 export default router;
